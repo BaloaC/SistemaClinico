a1f5fe30959707c13d57c6ed1bd2cc6fb856da35
Enrique Chacón <enrique.chacon0230@gmail.com>
2022-11-06 15:55:27 -0400

    subiendo archivos css/js

---
+++ assets/js/login/iniciarSesion.js
@@ -0,0 +1,68 @@
+import Cookies from "../js.cookie.min.js";
+
+const d = document,
+    $alert = d.querySelector(".alert"),
+    path = location.pathname.split('/');
+
+const logIn = async (form) => {
+
+    try {
+
+        const credentials = new FormData(form);
+
+        const options = {
+
+            method: "POST",
+            headers: { "Content-type": "application/json;charset=utf-8" },
+            body: JSON.stringify({
+                nombre: credentials.get("nombre"),
+                clave: credentials.get("clave"),
+            })
+        }
+
+        let response = await fetch(`/${path[1]}/login`, options),
+            json = await response.json();
+
+
+
+        if (!json.code) throw { result: json };
+
+        $alert.classList.remove("alert-danger");
+        $alert.classList.remove("d-none");
+        $alert.classList.add("alert-success");
+        $alert.textContent = "Sesión iniciada correctamente!";
+
+        Cookies.set("tokken", json.data.tokken);
+        Cookies.set("usuario", credentials.get("nombre"));
+        Cookies.set("usuario_id", json.data.usuario_id);
+        Cookies.set("rol", json.data.rol);
+
+        setTimeout(() => {
+            
+            location = `/${path[1]}/home`;
+        }, 1000);
+
+    } catch (error) {
+
+        $alert.classList.remove("d-none");
+        $alert.classList.add("alert-danger");
+        $alert.textContent = "El usuario o la contraseña son inválidos";
+
+    }
+
+}
+
+d.addEventListener("submit", e => {
+
+    if (e.target.matches(".login-form")) {
+
+        e.preventDefault();
+
+        logIn(e.target);
+    }
+})
+
+d.addEventListener("DOMContentLoaded", e => {
+
+    // validateSession();
+})
