fc606ca52ef77664e07d197154df1a96a2532f34
BaloaC <7mayini22@gmail.com>
2023-08-12 15:12:56 -0400

    Subiendo arreglos y refactorización en el post de consulta_seguro

--- controllers/ConsultaSeguroController.php
+++ controllers/ConsultaSeguroController.php
@@ -5,6 +5,8 @@ use PSpell\Config;
 include_once "./services/facturas/consulta seguro/ConsultaSeguroValidaciones.php";
 include_once "./services/facturas/consulta seguro/ConsultaSeguroService.php";
 
+include_once "./services/pacientes/paciente seguro/PacienteSeguroService.php";
+
 class ConsultaSeguroController extends Controller{
 
     protected $selectConsultas = array(
@@ -46,7 +48,7 @@ class ConsultaSeguroController extends Controller{
         "medico" => "cita",
         "especialidad" => "cita"
     );
-    
+
     //Método index (vista principal)
     public function index(){
 
@@ -59,207 +61,77 @@ class ConsultaSeguroController extends Controller{
     }
 
     public function formActualizarConsultaSeguro($consulta_seguro_id){
-        
+
         return $this->view('facturas/consulta_seguro/actualizarFacturas', ['factura_id' => $consulta_seguro_id]);
-    } 
+    }
 
     public function insertarConsultaSeguro(/*Request $request*/){
+
         $_POST = json_decode(file_get_contents('php://input'), true);
-        $validarConsulta = new Validate;
-        $camposNumericos = array('monto');
-        $camposId = array('consulta_id');
+        ConsultaSeguroValidaciones::validarConsultaSeguro($_POST);        
         
-        switch ($validarConsulta) {
-            case ($validarConsulta->isEmpty($_POST)):
-                $respuesta = new Response('DATOS_VACIOS');
-                return $respuesta->json(400);
+        $validarConsulta = new Validate;
+        $data = $validarConsulta->dataScape($_POST);
+        $_consultaSeguroModel = new ConsultaSeguroModel();
 
-            case $validarConsulta->isNumber($_POST, $camposNumericos):
-                $respuesta = new Response('DATOS_INVALIDOS');
-                return $respuesta->json(400);
+        // Obtengo la consulta para extraer el paciente y buscarlo en paciente_seguro
+        $_consultaCita = new ConsultaCitaModel();
+        $consulta = $_consultaCita->where('consulta_id', '=', $data['consulta_id'])->getFirst();
 
-            case !$validarConsulta->existsInDB($_POST, $camposId):
-                $respuesta = new Response('NOT_FOUND');
-                return $respuesta->json(200);
+        if ( is_null($consulta) ){
+            // Si no es consulta por cita, es consulta por emergencia
+            ConsultaSeguroValidaciones::validarConsultaEmergencia($data);
+            ConsultaSeguroService::insertarConsultaEmergencia($data);
+            ConsultaSeguroService::actualizarEstatusConsulta($data['consulta_id']);
 
-            case $validarConsulta->isDuplicated('consulta', 'estatus_con', '3'):
-                $respuesta = new Response(false, 'Esa consulta ya está relacionada a una factura');
-                return $respuesta->json(400);
+            $respuesta = new Response('INSERCION_EXITOSA');
+            return $respuesta->json(201);
+
+        } else {
+
+            $_cita = new CitaModel();
+            $cita = $_cita->where('cita_id', '=', $consulta->cita_id)->getFirst();
+
+            $_paciente = new PacienteModel();
+            $paciente = $_paciente->where('cedula', '=', $cita->cedula_titular)->getFirst();
             
-            default:
-            
-                $data = $validarConsulta->dataScape($_POST);
-                $_consultaSeguroModel = new ConsultaSeguroModel();
+            $_citaSeguro = new CitaSeguroModel();
+            $citaSeguro = $_citaSeguro->where('cita_id', '=', $consulta->cita_id)->getFirst();
+            $data['seguro_id'] = $citaSeguro->seguro_id;
 
-                // Obtengo la consulta para extraer el paciente y buscarlo en paciente_seguro
-                $_consultaCita = new ConsultaCitaModel();
-                $consulta = $_consultaCita->where('consulta_id', '=', $data['consulta_id'])->getFirst();
-                
-                if ( is_null($consulta) ){
+            $_pacienteSeguro = new PacienteSeguroModel();
+            $pacienteSeguro = $_pacienteSeguro->where('paciente_id', '=', $paciente->paciente_id)->where('seguro_id', '=', $citaSeguro->seguro_id)->getFirst();
 
-                    ConsultaSeguroValidaciones::validarConsultaEmergencia($data);
-                    ConsultaSeguroService::insertarConsultaEmergencia($data);
-                    ConsultaSeguroService::actualizarEstatusConsulta($data['consulta_id']);
-                    
-                    $respuesta = new Response('INSERCION_EXITOSA');
-                    return $respuesta->json(201);
+            if ($data['monto'] > $pacienteSeguro->saldo_disponible) {
+                $respuesta = new Response(false, 'Saldo insuficiente para cubrir la consulta');
+                $respuesta->setData("Error al procesar al paciente id $pacienteSeguro->paciente_id con saldo $pacienteSeguro->saldo_disponible");
+                return $respuesta->json(400);
+            }
 
-                } else if ( count($consulta) > 0 ) {
+            $id = $_consultaSeguroModel->insert($data);
+            $mensaje = ($id > 0);
 
-                    $_cita = new CitaModel();
-                    $cita = $_cita->where('cita_id', '=', $consulta->cita_id)->getFirst();
-                    $_paciente = new PacienteModel();
-                    $paciente = $_paciente->where('cedula', '=', $cita->cedula_titular)->getFirst();
-                    
-                    $_citaSeguro = new CitaSeguroModel();
-                    $citaSeguro = $_citaSeguro->where('cita_id', '=', $consulta->cita_id)->getFirst();
-                    $data['seguro_id'] = $citaSeguro->seguro_id;
+            if (!$mensaje) {
+                $respuesta = new Response(false, 'Error insertando la factura de la consulta');
+                return $respuesta->json(400);
+            }
 
-                    $_pacienteSeguro = new PacienteSeguroModel();
-                    $pacienteSeguro = $_pacienteSeguro->where('paciente_id', '=', $paciente->paciente_id)->where('seguro_id', '=', $citaSeguro->seguro_id)->getFirst();
+            // ya insertada la factura, modificamos el estatus de la consulta a pagada
+            ConsultaSeguroService::actualizarEstatusConsulta($data['consulta_id']);
 
-                    if ($data['monto'] > $pacienteSeguro->saldo_disponible) {
-                        $respuesta = new Response(false, 'Saldo insuficiente para cubrir la consulta');
-                        $respuesta->setData("Error al procesar al paciente id $pacienteSeguro->paciente_id con saldo $pacienteSeguro->saldo_disponible");
-                        return $respuesta->json(400);
-                    }
-
-                    $id = $_consultaSeguroModel->insert($data);
-                    $mensaje = ($id > 0);
-
-                    // ya insertada la factura, modificamos el estatus de la consulta a pagada
-                    ConsultaSeguroService::actualizarEstatusConsulta($data['consulta_id']);
-                    // $_consulta = new ConsultaModel();
-                    // $update = $_consulta->where('consulta_id','=',$data['consulta_id']  )->update(['estatus_con' => 3]);
-                    // $isUpdate = ($update > 0);
-
-                    // if (!$isUpdate) {
-                    //     // Si hubo un error cambiando el estatus de la consulta, borramos la factura relacionada a ella
-                    //     $_consultaSeguroModel = new ConsultaSeguroModel();
-                    //     $_consultaSeguroModel->where('consulta_seguro_id', '=', $id)->delete();
-
-                    //     $respuesta = new Response(false, 'Hubo un error actualizando el estatus de la consulta');
-                    //     $respuesta->setData('Error al colocar la consulta id '.$data['consulta_id'].' como cancelada');
-                    //     return $respuesta->json(400);
-                    // }
-                    
-                    if ($mensaje) {
-                        
-                        $montoActualizado = $pacienteSeguro->saldo_disponible - $data['monto'];
-                        $respuesta = $_pacienteSeguro->update([ 'saldo_disponible' => $montoActualizado ]);
-                        if (!$respuesta) {
-                            $respuesta = new Response(false, 'Hubo un error manipulando el saldo del paciente');
-                            $respuesta->setData('Error al procesar al paciente id ' + $paciente->paciente_id + 'con saldo ' + $paciente->saldo_disponible);
-                            return $respuesta->json(400);
-                        }
-
-                        $respuesta = new Response('INSERCION_EXITOSA');
-                        return $respuesta->json(201);
-
-                    } else {
-                        $respuesta = new Response('INSERCION_FALLIDA');
-                        return $respuesta->json(400);
-                    }
-
-                }
-                
-                // Restando el monto de la factura al saldo disponible del paciente
-                
-                //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                // $consulta_id = $data['consulta_id'];
-                
-                // $_consultaController = new ConsultaController();
-                // $consultaArray = $_consultaController->listarConsultaPorId($consulta_id);
-                // $json = json_decode($consultaArray);
-                // $consulta = $json->data['0'];
-                
-                
-                // validando que el paciente sea asegurado
-                // if (!$validarConsulta->isDuplicated('paciente_seguro', 'paciente_id', $consulta->paciente_id)) {
-                //     $respuesta = new Response(false, 'El paciente de la consulta no es asegurado');
-                //     return $respuesta->json(400);
-                // }
-
-                // ID para obtener el resto de la información
-                // $cita = $consulta->cita_id;
-                // $_citaModel = new CitaModel();
-                // $citaInfo = $_citaModel->where('cita_id','=',$cita)->getFirst();
-                
-                // //Obtener el nombre del paciente titular por la cita
-                // $_pacienteModel = new PacienteModel();
-                // $CItitular = $citaInfo->cedula_titular;
-                // $pacienteTitular = $_pacienteModel->where('cedula','=',$CItitular)->getFirst();
-                
-                // //Obtener el id del seguro
-                // $_seguroModel = new SeguroModel();
-                // $seguroId = $citaInfo->seguro_id;
-                // // $seguroInfo = $_seguroModel->where('seguro_id','=',$seguroId)->getFirst();
-
-                // // sacando el límite de la fecha
-                // $fecha_limite = strtotime('+30 day', strtotime($consulta->fecha_consulta,));
-                // $fecha_limite = date('Y-m-d', $fecha_limite);
-
-                // $insert = array(
-                //     'consulta_id' => $data['consulta_id'],
-                //     'seguro_id' => $seguroId,
-                //     'tipo_servicio' => $data['tipo_servicio'],
-                //     'monto' => $data['monto'],
-                //     'fecha_ocurrencia'=> $consulta->fecha_consulta,
-                //     'especialidad_id' => $consulta->especialidad_id,
-                //     'nombre_especialidad' => $consulta->nombre_especialidad,
-                //     'nombre_paciente' => $consulta->nombre_paciente,
-                //     'nombre_titular' => $pacienteTitular->nombre,
-                //     'autorizacion' => $consulta->clave,
-                //     'fecha_pago_limite' => $fecha_limite
-                // );
-                
-                // // validando que el paciente pueda cubrir el saldo
-                // $_pacienteSeguroModel = new PacienteSeguroModel;
-                // $paciente = $_pacienteSeguroModel->where('estatus_pac','=',1)->where('paciente_id', '=',$pacienteTitular->paciente_id)->where('seguro_id','=',$seguroId)->getFirst();
-                // $saldo = isset($paciente->saldo_disponible) ? $paciente->saldo_disponible : 0;
-                
-                // if ($data['monto'] > $saldo) {
-                //     $respuesta = new Response('INSUFFICIENT_AMOUNT');
-                //     return $respuesta->json(400);
-                // }
-
-                // $_consultaSeguroModel = new ConsultaSeguroModel();
-                // $_consultaSeguroModel->byUser($token);
-                // $id = $_consultaSeguroModel->insert($insert);
-
-                // $mensaje = ($id > 0);
-                
-                // if ($mensaje) {
-                    
-                //     // Restando el monto de la factura al saldo disponible del paciente
-                //     $montoActualizado = $saldo - $data['monto'];
-                //     $update = array('saldo_disponible' => $montoActualizado);
-                //     $respuesta = $_pacienteSeguroModel->where('paciente_id', '=',$pacienteTitular->paciente_id)->update($update);
-
-                //     if (!$respuesta) {
-                //         $respuesta = new Response(false, 'Hubo un error manipulando el saldo del paciente');
-                //         return $respuesta->json(400);
-                //     }
-
-                //     $respuesta = new Response('INSERCION_EXITOSA');
-                //     return $respuesta->json(201);
-
-                // } else {
-
-                //     $respuesta = new Response('INSERCION_FALLIDA');
-                //     return $respuesta->json(400);
-                // }
+            $montoActualizado = $pacienteSeguro->saldo_disponible - $data['monto'];
+            PacienteSeguroService::actualizarSaldoPaciente($montoActualizado, $_pacienteSeguro);
         }
     }
 
     public function listarConsultaSeguro(){
-        
+
         $_consultaSeguroModel = new ConsultaSeguroModel();
-        
+
         // Hacemos el inner de la información general
         $innersConsulta = $_consultaSeguroModel->listInner($this->innerConsulta);
         $ConsultasSeguros = $_consultaSeguroModel->innerJoin($this->selectConsultas, $innersConsulta, "consulta_seguro");
-        
+
         if (count($ConsultasSeguros)) {
             foreach ($ConsultasSeguros as $consulta) {
                 $consulta = $this->obtenerInformacion($consulta);
@@ -281,7 +153,7 @@ class ConsultaSeguroController extends Controller{
 
         $consultasSeguro = json_decode($this->listarConsultaSeguro());
         $factura = array_filter($consultasSeguro->data, fn($consulta) => $consulta->consulta_seguro_id == $consulta_seguro_id);
-        
+
         $isExist = count($factura) > 0;
         $respuesta = new Response($factura ? 'CORRECTO' : 'ERROR');
         $respuesta->setData($factura);
@@ -291,7 +163,7 @@ class ConsultaSeguroController extends Controller{
     public function eliminarConsultaSeguro($consulta_seguro_id){
 
         $validarConsulta = new Validate;
-        
+
         $_consultaSeguroModel = new ConsultaSeguroModel();
         $data = array(
             'estatus_con' => '2'
--- services/facturas/consulta seguro/ConsultaSeguroValidaciones.php
+++ services/facturas/consulta seguro/ConsultaSeguroValidaciones.php
@@ -3,6 +3,38 @@
 class ConsultaSeguroValidaciones {
 
     /**
+     * Validaciones básicas de consulta_seguro
+     */
+    public static function validarConsultaSeguro($formulario) {
+        $validarConsulta = new Validate;
+        $camposNumericos = array('monto');
+        $camposId = array('consulta_id');
+
+        switch ($validarConsulta) {
+            case ($validarConsulta->isEmpty($formulario)):
+                $respuesta = new Response('DATOS_VACIOS');
+                echo $respuesta->json(400);
+                exit();
+
+            case $validarConsulta->isNumber($formulario, $camposNumericos):
+                $respuesta = new Response('DATOS_INVALIDOS');
+                echo $respuesta->json(400);
+                exit();
+
+            case !$validarConsulta->existsInDB($formulario, $camposId):
+                $respuesta = new Response('NOT_FOUND');
+                echo $respuesta->json(200);
+                exit();
+
+            case $validarConsulta->isDuplicatedId('consulta_id', 'estatus_con', $formulario['consulta_id'], '3', 'consulta'):
+            // case $validarConsulta->isDuplicated('consulta', 'estatus_con', '3'):
+                $respuesta = new Response(false, 'Esa consulta ya está relacionada a una factura');
+                echo $respuesta->json(400);
+                exit();
+        }
+    }
+
+    /**
      * Validaciones para insertar factura de consulta por emergencia
      */
     public static function validarConsultaEmergencia($formulario) {
