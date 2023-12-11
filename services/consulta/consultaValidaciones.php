f29ce9ca59796520b22e292c6c2bed19bd22427b
BaloaC <7mayini22@gmail.com>
2023-12-10 22:40:17 -0400

    descomentando validación arreglada con la clave de la cita

--- services/consulta/consultaValidaciones.php
+++ services/consulta/consultaValidaciones.php
@@ -191,11 +191,11 @@ class ConsultaValidaciones {
             exit();
         } 
         
-        // if ( !$validarConsulta->isDuplicatedId('cita_id', 'estatus_cit', $formulario['cita_id'], 3, 'cita') ) {
-        //     $respuesta = new Response(false, 'Para realizar la consulta la cita debe tener su clave correspondiente');
-        //     echo $respuesta->json(400);
-        //     exit();
-        // }
+        if ( !$validarConsulta->isDuplicatedId('cita_id', 'estatus_cit', $formulario['cita_id'], 3, 'cita') ) {
+            $respuesta = new Response(false, 'Para realizar la consulta la cita debe tener su clave correspondiente');
+            echo $respuesta->json(400);
+            exit();
+        }
     }
 
     /**
