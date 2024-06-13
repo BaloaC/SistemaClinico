b60e2a7d522bdbdbf0f82bd0db4fa12e998275db
BaloaC <7mayini22@gmail.com>
2024-06-12 22:23:20 -0400

    enviando en el cita by id el nombre de la empresa y el seguro

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -1,23 +1,23 @@
 <?php
 
-class LoginController extends Controller {
+class LoginController extends Controller{
 
     //Método index (vista principal)
-    public function index() {
+    public function index(){
 
         return $this->view('login/index');
     }
 
-    public function recuperarUsuarioView() {
+    public function recuperarUsuarioView(){
 
         return $this->view('login/recuperarUsuario.php');
     }
 
-    public function entrar() {
+    public function entrar(){
 
         $_POST = json_decode(file_get_contents('php://input'), true);
         $camposKey = array("nombre", "clave");
-
+        
         $validarLogin = new Validate;
 
         if ($validarLogin->isEmpty($_POST)) {
@@ -25,61 +25,7 @@ class LoginController extends Controller {
         }
 
         $_UsuarioModel = new UsuarioModel();
-        $usuario = $_UsuarioModel->where('nombre', '=', $_POST["nombre"])->getFirst();
-
-        if (!is_null($usuario)) {
-            if ($usuario->estatus_usu == 2) {
-                $respuesta = new Response(false, 'Su usuario se encuentar deshabilitado');
-                echo $respuesta->json(400);
-                exit();
-            } else {
-
-                $claveEncriptada = $usuario->clave;
-                $clave = $_POST["clave"];
-
-                if (password_verify($clave, $claveEncriptada)) {
-                    $code = bin2hex(random_bytes(5));
-                    $tokken = array('tokken' => $code);
-
-                    $_UsuarioModel = new UsuarioModel();
-                    $actualizado = $_UsuarioModel->where('nombre', '=', $_POST['nombre'])->update($tokken);
-                    $mensaje = ($actualizado > 0);
-
-                    $tokken['usuario_id'] = $usuario->usuario_id;
-                    $tokken['rol'] = $usuario->rol;
-                    $tokken['nombres'] = $usuario->nombres;
-                    $tokken['apellidos'] = $usuario->apellidos;
-
-                    // Automatización de facturas_seguro
-                    $_facturaSeguro = new FacturaSeguroController();
-                    // $_facturaSeguro->insertarFacturaSeguro();
-
-                    $respuesta = new Response($mensaje ? 'CORRECTO' : 'ERROR');
-                    $respuesta->setData($tokken);
-
-                    return $respuesta->json(200);
-                } else {
-                    return $respuesta = new Response('DATOS_INVALIDOS');
-                }
-            }
-        }
-
-        return $respuesta = new Response('DATOS_INVALIDOS');
-    }
-
-    public function validarUsuario() {
-        $_POST = json_decode(file_get_contents('php://input'), true);
-        $camposKey = array("nombre", "clave");
-
-        $validarLogin = new Validate;
-
-        if ($validarLogin->isEmpty($_POST)) {
-            return $respuesta = new Response('DATOS_INVALIDOS');
-        }
-
-        $_UsuarioModel = new UsuarioModel();
-        $usuario = $_UsuarioModel->where('nombre', '=', $_POST["nombre"])->getFirst();
-
+        $usuario = $_UsuarioModel->where('nombre','=',$_POST["nombre"])->getFirst();
         
         if (!is_null($usuario)) {
             if ($usuario->estatus_usu == 2) {
@@ -87,66 +33,119 @@ class LoginController extends Controller {
                 echo $respuesta->json(400);
                 exit();
             } else {
-                    
+                
                 $claveEncriptada = $usuario->clave;
                 $clave = $_POST["clave"];
-                
-                if (password_verify($clave, $claveEncriptada)) {
-                    $tokken = [];
+    
+                if(password_verify($clave, $claveEncriptada)){
+                    $code = bin2hex(random_bytes(5));
+                    $tokken = array( 'tokken' => $code);
+    
+                    $_UsuarioModel = new UsuarioModel();
+                    $actualizado = $_UsuarioModel->where('nombre','=',$_POST['nombre'])->update($tokken);
+                    $mensaje = ($actualizado > 0);
+                    
                     $tokken['usuario_id'] = $usuario->usuario_id;
                     $tokken['rol'] = $usuario->rol;
-                    $tokken['nombres'] = $usuario->nombres;
-                    $tokken['apellidos'] = $usuario->apellidos;
-                    
-                    $respuesta = new Response('CORRECTO');
+    
+                    // Automatización de facturas_seguro
+                    $_facturaSeguro = new FacturaSeguroController();
+                    // $_facturaSeguro->insertarFacturaSeguro();
+    
+                    $respuesta = new Response($mensaje ? 'CORRECTO' : 'ERROR');
+                    $respuesta->setData($tokken);
+    
                     return $respuesta->json(200);
-                    
+    
                 } else {
                     return $respuesta = new Response('DATOS_INVALIDOS');
                 }
             }
         }
-
+        
         return $respuesta = new Response('DATOS_INVALIDOS');
     }
 
-    public function recuperarUsuario($usuario_id) {
-
+    public function validarUsuario() {
         $_POST = json_decode(file_get_contents('php://input'), true);
+        $camposKey = array("nombre", "clave");
+        
+        $validarLogin = new Validate;
 
+        if ($validarLogin->isEmpty($_POST)) {
+            return $respuesta = new Response('DATOS_INVALIDOS');
+        }
+
+        $_UsuarioModel = new UsuarioModel();
+        $usuario = $_UsuarioModel->where('nombre','=',$_POST["nombre"])->getFirst();
+
+        if (!is_null($usuario)) {
+            if ($usuario->estatus_usu == 2) {
+                $respuesta = new Response('DATOS_INVALIDOS');
+                echo $respuesta->json(400);
+                exit();
+                
+            } else {
+                
+                $claveEncriptada = $usuario->clave;
+                $clave = $_POST["clave"];
+    
+                if(password_verify($clave, $claveEncriptada)){   
+                    $respuesta = new Response('CORRECTO');    
+                    echo $respuesta->json(200);
+                    exit();
+    
+                } else {
+                    $respuesta = new Response('DATOS_INVALIDOS');
+                    echo $respuesta->json(400);
+                    exit();
+                }
+            }
+        }
+        
+        $respuesta = new Response('DATOS_INVALIDOS');
+        echo $respuesta->json(400);
+        exit();
+    }
+    
+    public function recuperarUsuario($usuario_id) {
+        
+        $_POST = json_decode(file_get_contents('php://input'), true);
+        
         // if ($_POST['tipo_auth'] > 2) {
-
+            
         //     $respuesta = new Response(false, 'Método de autenticación incorrecto');
         //     return $respuesta->json(400);
 
         // } else if ($_POST['tipo_auth'] == 1) {
 
-        // if ( !($_POST['auth']) ) {
+            // if ( !($_POST['auth']) ) {
+                
+            //     $respuesta = new Response(false, 'El pin debe estar compuesto únicamente por números');
+            //     return $respuesta->json(400);
 
-        //     $respuesta = new Response(false, 'El pin debe estar compuesto únicamente por números');
-        //     return $respuesta->json(400);
+            // } else {
 
-        // } else {
+                $_UsuarioModel = new UsuarioModel();
+                $usuario = $_UsuarioModel->where('usuario_id','=',$usuario_id)->getFirst();
+                $pinEncriptada = $usuario->pin;
+                $clave = $_POST['auth'];
+                
+                if (password_verify($clave, $pinEncriptada)) {
+                    
+                    $actualizado['clave'] = password_hash($_POST['nueva_clave'], PASSWORD_DEFAULT);
+                    $_UsuarioModel = new UsuarioModel();
+                    $cambio = $_UsuarioModel->where('usuario_id','=',$usuario_id)->update($actualizado);
 
-        $_UsuarioModel = new UsuarioModel();
-        $usuario = $_UsuarioModel->where('usuario_id', '=', $usuario_id)->getFirst();
-        $pinEncriptada = $usuario->pin;
-        $clave = $_POST['auth'];
+                    $respuesta = new Response($cambio ? 'INSERCION_EXITOSA' : 'INSERCION_FALLIDA');
+                    return $respuesta->json(200);
 
-        if (password_verify($clave, $pinEncriptada)) {
-
-            $actualizado['clave'] = password_hash($_POST['nueva_clave'], PASSWORD_DEFAULT);
-            $_UsuarioModel = new UsuarioModel();
-            $cambio = $_UsuarioModel->where('usuario_id', '=', $usuario_id)->update($actualizado);
-
-            $respuesta = new Response($cambio ? 'INSERCION_EXITOSA' : 'INSERCION_FALLIDA');
-            return $respuesta->json(200);
-        } else {
-
-            $respuesta = new Response(false, 'Pin incorrecto');
-            return $respuesta->json(400);
-        }
-        // }
+                } else {
+                    
+                    $respuesta = new Response(false, 'Pin incorrecto');
+                    return $respuesta->json(400);
+                }
+            // }
         // } else if ($_POST['tipo_auth'] == 2) {
 
         //     return 'pq cae aki';
@@ -158,26 +157,26 @@ class LoginController extends Controller {
         $_POST = json_decode(file_get_contents('php://input'), true);
         $validarUsuario = new Validate;
 
-        switch ($_POST) {
+        switch($_POST) {
             case ($validarUsuario->isEmpty($_POST)):
                 $respuesta = new Response('DATOS_INVALIDOS');
                 return $respuesta->json(400);
-
+        
             case !$validarUsuario->isDuplicated("usuario", 'usuario_id', $usuario_id):
                 $respuesta = new Response('NOT_FOUND');
                 return $respuesta->json(200);
 
-            default:
+            default: 
 
                 foreach ($_POST as $posts) {
-
+                    
                     $data = $validarUsuario->dataScape($posts);
                     $data['usuario_id'] = $usuario_id;
 
                     $_preguntaSeguridadModel = new PreguntaSeguridadModel();
                     $actualizado = $_preguntaSeguridadModel->insert($data);
                     $mensaje = ($actualizado > 0);
-
+            
                     if (!$mensaje) {
 
                         $respuesta = new Response('INSERCION_FALLIDA');
@@ -186,8 +185,8 @@ class LoginController extends Controller {
                     }
                 }
 
-                $respuesta = new Response('INSERCION_EXITOSA');
-                return $respuesta->json(201);
+            $respuesta = new Response('INSERCION_EXITOSA');
+            return $respuesta->json(201);
         }
     }
 
@@ -195,34 +194,34 @@ class LoginController extends Controller {
 
         $_POST = json_decode(file_get_contents('php://input'), true);
         $validarUsuario = new Validate;
-
-        switch ($_POST) {
-
+        
+        switch($_POST) {
+        
             case !$validarUsuario->isDuplicated("usuario", "usuario_id", $usuario_id):
                 $respuesta = new Response('NOT_FOUND');
                 return $respuesta->json(200);
 
-            default:
-
+            default: 
+   
                 $usuario_id = $usuario_id;
                 $pregunta = $_POST['preguntas'];
 
                 foreach ($pregunta as $preguntas) {
-
+                    
                     $_preguntaSeguridadModel = new PreguntaSeguridadModel();
                     $select = $_preguntaSeguridadModel->where('pregunta', '=', $preguntas['pregunta'])->getFirst();
                     $respuesta = $select->respuesta;
-
+                    
                     if ($preguntas['respuesta'] != $respuesta) {
-
+                        
                         $respuesta = new Response(false, 'Respuesta de seguridad incorrecta');
                         $respuesta->setData($preguntas['pregunta']);
                         return $respuesta->json(400);
                     }
                 }
-
+                
                 $insert['clave'] = password_hash($_POST['nueva_clave'], PASSWORD_DEFAULT);
-
+                
                 $_usuarioModel = new UsuarioModel();
                 $mensaje = $_usuarioModel->where('usuario_id', '=', $usuario_id)->update($insert);
 
