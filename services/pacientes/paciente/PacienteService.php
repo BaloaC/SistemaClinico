36a5019a1fcc483c3155f5fa4b297b7f2b248ff3
BaloaC <7mayini22@gmail.com>
2024-06-25 22:43:08 -0400

    arreglando error co pacientes beneficiados

--- services/pacientes/paciente/PacienteService.php
+++ services/pacientes/paciente/PacienteService.php
@@ -41,9 +41,22 @@ class PacienteService{
 
         } else if( $formulario['tipo_paciente'] == 4 ) {
             // Lógica para paciente tipo beneficiado
+
             $pacienteBeneficiado = $formulario['titular'];
             PacienteValidaciones::validarPacienteBeneficiado($pacienteBeneficiado);
-            $cedula_nueva = PacienteHelpers::retornarCedulaFormateada($formulario);
+
+            // validamos si la cédula pertenece a alguien más
+            $cedula_beneficiado = 0;
+            $i = 0;
+
+            while($cedula_beneficiado != $formulario['cedula']) {
+                $_pacienteModel = new PacienteModel();
+                $paciente_titular = $_pacienteModel->where('paciente_id', '=', $formulario['titular'][$i]['paciente_id'])->getFirst();
+
+                if ( $paciente_titular->cedula == $formulario['cedula'] ) {
+                    $cedula_nueva = PacienteHelpers::retornarCedulaFormateada($formulario);
+                }
+            }
             
             if (!is_null($cedula_nueva)) {
                 $formulario['cedula'] = $cedula_nueva;
